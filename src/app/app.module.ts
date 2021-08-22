9bbd0adc7fb61a39fb6dfd983a839d0f71414341
Bharanidharan2K <bharanikkab123@gmail.com>
2021-08-22 23:12:53 +0530

    Shopping cart view completed filters, nav, cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
+import { CartListComponent } from './components/shopping-cart/cart-list/cart-list.component';
+import { CartItemComponent } from './components/shopping-cart/cart-list/cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartListComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule
