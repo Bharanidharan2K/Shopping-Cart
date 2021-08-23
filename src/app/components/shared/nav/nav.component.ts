675263aa92c44c4a451ff6efcafe05c27588be80
Bharanidharan2K <bharanikkab123@gmail.com>
2021-08-23 22:14:58 +0530

    cart icon value added sucessfull

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { MessengerService } from 'src/app/services/messenger.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  price : number = 0;
+  totalQty: number = 0;
+  constructor(private msg: MessengerService) { }
 
   ngOnInit(): void {
+    this.msg.getMessage().subscribe((product: any) => {
+      this.addProductToCart(product);
+    })
+
+  }
+
+  addProductToCart(product : Product){
+    this.price += product.price;
+    this.totalQty ++;
   }
 
 }
