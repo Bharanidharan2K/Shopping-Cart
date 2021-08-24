98f7cc5e5a0509416ce8fc1be373bb4ca3ddcf0b
Bharanidharan2K <bharanikkab123@gmail.com>
2021-08-25 00:25:59 +0530

    update github api for products and cart

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,6 +1,10 @@
 import { environment } from "src/environments/environment";
 
 
-export const baseUrl = environment.production ? '' : 'http://localhost:3000'
-export const productUrl = baseUrl+'/products'
-export const cartUrl = baseUrl+'/cart'
+// export const baseUrl = environment.production ? '' : 'http://localhost:3000'
+// export const productUrl = baseUrl+'/products'
+// export const cartUrl = baseUrl+'/cart'
+
+export const baseUrl = environment.production ? '' : 'https://bharanidharan2k.github.io/shopping-cart-json-api'
+export const productUrl = 'https://bharanidharan2k.github.io/shopping-cart-json-api/products.json'
+export const cartUrl = 'https://bharanidharan2k.github.io/shopping-cart-json-api/cart.json'
